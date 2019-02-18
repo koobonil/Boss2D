1abd50beda6c5f4b4f6f70f1ad37a4eabde775a4
BonHyukGu <slacealic@wonderpeople.co.kr>
2019-02-18 19:41:43 +0900

    1.2.13
    일부 맥PC용 malloc패치

--- Boss2D/addon/trick_for_fakewin/malloc.h
+++ Boss2D/addon/trick_for_fakewin/malloc.h
@@ -1,1 +1,9 @@
 ﻿#pragma once
+
+extern "C"
+{
+    extern void *malloc(size_t size);
+    extern void *calloc(size_t size);
+    extern int free(void *ptr); 
+    extern void *realloc(void *ptr, size_t size);
+}
